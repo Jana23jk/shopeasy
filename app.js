867a4800ce4fc89b075ce8293d3e17bd6561eec9
Jana23jk <janakhan2323@gmail.com>
2025-07-21 23:14:38 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const name = "codetest"
 app.get("/",(req,res)=>{
-    res.send("Welcome to ShopEasy! from codetest");
+    const message = `Welcome to ShopEasy! from ${name}!`
+    res.send(message);
 })
 
 const server = app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
 
-module.exports = {app,server}
+module.exports = {app,server,name}
